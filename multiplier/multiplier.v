5e51f764c84397fabb5d1e45052acc2a09832755
SzymonOzog <szymon.ozog@gmail.com>
2025-12-23 20:59:08 +0100

    Make it a simple multiplier, add overflow marker and more tests

--- multiplier/multiplier.v
+++ multiplier/multiplier.v
@@ -1,7 +1,8 @@
 module multiplier_int8(
         input wire[7:0] a,
         input wire[7:0] b,
-        output wire[7:0] y
+        output wire[7:0] y,
+        output wire overflow 
         );
 
         wire[7:0] a0;
@@ -22,34 +23,32 @@ module multiplier_int8(
         assign a6 = b[6] ? a : 8'b0;
         assign a7 = b[7] ? a : 8'b0;
 
-        wire[7:0] y0;
-        wire[7:0] y1;
-        wire[7:0] y2;
-        wire[7:0] y3;
+        wire[8:0] y0;
+        wire[8:0] y1;
+        wire[8:0] y2;
+        wire[8:0] y3;
+        wire[8:0] y4;
+        wire[8:0] y5;
+        wire[8:0] y6;
+        wire[8:0] y7;
 
         
-        wire[6:0] carry;
-        ripple_adder8 adder0((a0>>1), a1, 1'b0, y0, carry[0]);
-        ripple_adder8 adder1(a2, a3, 1'b0, y1, carry[1]);
-        ripple_adder8 adder2(a4, a5, 1'b0, y2, carry[2]);
-        ripple_adder8 adder3(a6, a7, 1'b0, y3, carry[3]);
-
-        wire[7:0] y10;
-        wire[7:0] y11;
-
-        ripple_adder8 adder10((y0>>1), y1, 1'b0, y10, carry[4]);
-        ripple_adder8 adder11(y2, y3, 1'b0, y11, carry[5]);
-
-        wire[7:0] y20;
-
-        ripple_adder8 adder20((y10>>1), y11, 1'b0, y20, carry[6]);
+        ripple_adder8 adder0((a0>>1), a1, 1'b0, y0[7:0] ,y0[8]);
+        ripple_adder8 adder1(y0[8:1], a2, 1'b0, y1[7:0], y1[8]);
+        ripple_adder8 adder2(y1[8:1], a3, 1'b0, y2[7:0], y2[8]);
+        ripple_adder8 adder3(y2[8:1], a4, 1'b0, y3[7:0], y3[8]);
+        ripple_adder8 adder4(y3[8:1], a5, 1'b0, y4[7:0], y4[8]);
+        ripple_adder8 adder5(y4[8:1], a6, 1'b0, y5[7:0], y5[8]);
+        ripple_adder8 adder6(y5[8:1], a7, 1'b0, y6[7:0], y6[8]);
 
         assign y[0] = a0[0];
         assign y[1] = y0[0];
-        assign y[2] = y10[0];
-        assign y[3] = y20[0];
-        assign y[4] = y20[1];
-        assign y[5] = y20[2];
-        assign y[6] = y20[3];
-        assign y[7] = y20[4];
+        assign y[2] = y1[0];
+        assign y[3] = y2[0];
+        assign y[4] = y3[0];
+        assign y[5] = y4[0];
+        assign y[6] = y5[0];
+        assign y[7] = y6[0];
+
+        assign overflow = (y6[1] | y6[2] | y6[3] | y6[4] | y6[5] | y6[6] | y6[7] | y6[8]);
 endmodule
--- multiplier/tb.v
+++ multiplier/tb.v
@@ -2,8 +2,9 @@ module tb;
     reg[7:0] a;
     reg[7:0] b;
     wire[7:0] y;
+    wire overflow;
 
-    multiplier_int8 multiplier(a, b, y);
+    multiplier_int8 multiplier(a, b, y, overflow);
 
 
     initial begin
@@ -13,38 +14,50 @@ module tb;
         a = 8'd5;
         b = 8'd1;
         
-        #1 assert(y === 8'd5) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+        #1 assert(y === 8'd5 && overflow == 1'b0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, 8'd5);
 
         a = 8'd5;
         b = 8'd2;
         
-        #1 assert(y === 8'd10) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+        #1 assert(y === 8'd10 && overflow == 1'b0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, 8'd10);
 
         a = 8'd5;
         b = 8'd5;
         
-        #1 assert(y === 8'd25) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+        #1 assert(y === 8'd25 && overflow == 1'b0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, 8'd25);
 
         a = 8'd5;
         b = 8'd0;
         
-        #1 assert(y === 8'd0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+        #1 assert(y === 8'd0 && overflow == 1'b0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, 8'd0);
 
         a = 8'd0;
         b = 8'd5;
         
-        #1 assert(y === 8'd0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+        #1 assert(y === 8'd0 && overflow == 1'b0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, 8'd0);
 
         a = 8'd1;
         b = 8'd5;
         
-        #1 assert(y === 8'd5) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+        #1 assert(y === 8'd5 && overflow == 1'b0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, 8'd5);
+
+        a = 8'd10;
+        b = 8'd25;
+        
+        #1 assert(y === 8'd250 && overflow == 1'b0) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+            a, b, y, 8'd250);
+
+        a = 8'd12;
+        b = 8'd25;
+        
+        #1 assert(overflow == 1'b1) else $fatal(1, "wrong output for a %b b %b y=%b, overflow not detected", 
+            a, b, y);
 
         $display("All test passed");
         $finish;
