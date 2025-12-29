b6be501a484d60c2e079138e84876a3d955d6e3b
Szymon <szymon.ozog@gmail.com>
2025-12-29 19:44:51 +0100

    Working float8 adder

--- float_adder/float_adder.v
+++ float_adder/float_adder.v
@@ -2,57 +2,99 @@ module float_adder_e4m3(
         input wire[7:0] a,
         input wire[7:0] b,
         input wire clock,
+        input wire reset,
         output wire[7:0] y
         );
 
-        reg[3:0] a_e = a[6:3];
-        reg[3:0] b_e = b[6:3];
-        reg[3:0] y_e;
+        wire[3:0] a_e = a[6:3];
+        wire[3:0] b_e = b[6:3];
 
+        reg[3:0] a_e_aligned;
+        reg[3:0] b_e_aligned;
 
-        reg[3:0] a_m = {1'b1, a[2:0]};
-        reg[3:0] b_m = {1'b1, b[2:0]};
-        reg[2:0] y_m;
+        wire[3:0] a_m = {1'b1, a[2:0]};
+        wire[3:0] b_m = {1'b1, b[2:0]};
+        reg[3:0] a_m_aligned;
+        reg[3:0] b_m_aligned;
 
-        reg[3:0] diff;
+        reg[4:0] diff;
 
-        reg[2:0] shift_amt;
+        reg[3:0] shift_amt;
         reg sub_sign_change;
 
         reg[4:0] m_sum_tmp;
         reg[4:0] m_sum;
+        reg[4:0] m_sum_next;
+
         reg[3:0] e_sum;
+        reg[3:0] e_sum_next;
+
+        reg[1:0] curr_state;
+        reg[1:0] next_state;
+
+        parameter EXP = 2'd1;
+        parameter NORM = 2'd2;
 
         reg add_carry;
-
-        initial 
+        always @ (posedge clock or posedge reset)
+        begin
+            if (reset)
             begin
-                diff = a_e - b_e;
-                if (diff[3]) begin
-                    shift_amt = ~(diff[2:0]) + 1'b1;
-                    a_m = a_m >> shift_amt;
-                    a_e += shift_amt;
-                end else begin
-                    shift_amt = diff[2:0];
-                    b_m = b_m >> shift_amt;
-                    b_e += shift_amt;
+                curr_state <= EXP;
+                m_sum <= 4'd0;
+                e_sum <= 3'd0;
+            end
+            else
+            begin
+                curr_state <= next_state;
+                m_sum <= m_sum_next;
+                e_sum <= e_sum_next;
+            end
+        end
+
+        always @ (*)
+        begin
+            case(curr_state)
+                EXP:
+                begin
+                    diff = a_e - b_e;
+                    if (diff[4]) begin
+                        shift_amt = ~(diff[3:0]) + 1'b1;
+                        a_m_aligned = a_m >> shift_amt;
+                        a_e_aligned = a_e + shift_amt;
+                        b_m_aligned = b_m;
+                        b_e_aligned = b_e;
+                        e_sum_next = b_e;
+                    end 
+                    else
+                    begin
+                        shift_amt = diff[3:0];
+                        a_m_aligned = a_m;
+                        a_e_aligned = a_e;
+                        b_m_aligned = b_m >> shift_amt;
+                        b_e_aligned = b_e + shift_amt;
+                        e_sum_next = a_e;
+                    end
+                    m_sum_tmp = a[7] ? b_m_aligned - a_m_aligned : 
+                                b[7] ? a_m_aligned - b_m_aligned :
+                                       a_m_aligned + b_m_aligned;
+                    m_sum_next = m_sum_tmp[4] ? ~(m_sum_tmp) + 1'b1 : m_sum_tmp;
+                    next_state = NORM;
                 end
 
-                m_sum_tmp = a[7] ? b_m - a_m : 
-                            b[7] ? a_m - b_m :
-                                   a_m + b_m;
-
-                m_sum = m_sum_tmp[4] ? ~(m_sum_tmp) + 1'b1 : m_sum_tmp;
-                while (m_sum[3] != 1'b1)
+                NORM:
+                begin
+                    if (!m_sum[3])
                     begin
                         add_carry = m_sum[4] & !(a[7] & b[7]);
-                        m_sum = add_carry ? m_sum >> 1 : m_sum << 1;
-                        e_sum = add_carry ? e_sum + 1'b1 : e_sum - 1'b1;
+                        m_sum_next = add_carry ? m_sum >> 1 : m_sum << 1;
+                        e_sum_next = add_carry ? e_sum + 1'b1 : e_sum - 1'b1;
                     end
+                end
+            endcase
+        end
 
-            end
-
-        assign y[7] = (a[7] & b[7]) || m_sum_tmp[3];
+        assign y[7] = (a[7] & b[7]) || (m_sum_tmp[3] & (a[7] ^ b[7]));
         assign y[6:3] = e_sum;
         assign y[2:0] = m_sum[2:0];
 endmodule
--- float_adder/tb.v
+++ float_adder/tb.v
@@ -4,36 +4,45 @@ module tb;
     wire[7:0] y;
     reg[7:0] expected;
     reg clock;
+    reg reset;
 
-    float_adder_e4m3 adder(a, b, clock, y);
+    float_adder_e4m3 adder(a, b, clock, reset, y);
 
     always #1 clock = ~clock;
-
 
     initial begin
         $dumpfile("dump.vcd");
         $dumpvars(0, tb);
         clock = 1'b1;
+        reset = 1'b1;
 
         a = 8'b01000000;
         b = 8'b01000000;
         expected = 8'b01001000;
+        #1 reset = 1'b0;
         
-        #1 assert(y === expected) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+        #10 assert(y === expected) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, expected);
+
 
         a = 8'b00101000;
         b = 8'b00010000;
         expected = 8'b00101001;
-        
-        #1 assert(y === expected) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+
+        #1 reset = 1'b1;
+        #2 reset = 1'b0;
+
+        #10 assert(y === expected) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, expected);
 
         a = 8'b01010000;
         b = 8'b00010000;
         expected = 8'b01010000;
-        
-        #1 assert(y === expected) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+
+        #1 reset = 1'b1;
+        #2 reset = 1'b0;
+
+        #10 assert(y === expected) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, expected);
 
 
