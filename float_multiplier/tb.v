cc27180eafda9944aa7a494fe82dddf464b64625
Szymon <szymon.ozog@gmail.com>
2025-12-30 12:01:24 +0100

    testbench and makefile for multiplier

---
+++ float_multiplier/tb.v
@@ -0,0 +1,32 @@
+module tb;
+    reg[7:0] a;
+    reg[7:0] b;
+    wire[7:0] y;
+    reg[7:0] expected;
+    reg clock;
+    reg reset;
+    reg is_output_valid;
+
+    float_multiplier_e4m3 multiplier(a, b, clock, reset, y, is_output_valid);
+
+    always #1 clock = ~clock;
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, tb);
+        clock = 1'b1;
+        reset = 1'b1;
+
+        a = 8'b01000000;
+        b = 8'b01000000;
+        expected = 8'b01001000;
+        #1 reset = 1'b0;
+        
+        #10 assert(y === expected & is_output_valid) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
+            a, b, y, expected);
+
+        $display("All test passed");
+        $finish;
+    end
+endmodule
+
