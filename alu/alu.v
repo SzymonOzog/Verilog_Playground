d4fa30972c8bb309d7b55246c691a5fbfc4a089e
Szymon <szymon.ozog@gmail.com>
2026-01-06 08:25:41 +0100

    Simplify to just forward the result

--- alu/alu.v
+++ alu/alu.v
@@ -14,52 +14,14 @@ module alu(
         wire[7:0] add_y;
         wire add_valid;
 
-        reg[7:0] out;
-        reg[7:0] next_out;
-
-        reg valid; 
-        reg next_valid;
-
-        assign y = out;
-        assign is_output_valid = valid;
-
         float_adder_e4m3 adder(a, b, clock, reset, add_y, add_valid);
         float_multiplier_e4m3 multiplier(a, b, clock, reset, mul_y, mul_valid);
 
-        always @ (posedge clock or posedge reset)
-        begin
-            if (reset)
-            begin
-                out <= 8'd0;
-                next_out <= 8'd0;
-                valid <= 1'b0;
-                next_valid <= 1'b0;
-            end
-            else
-                out <= next_out;
-                valid <= next_valid;
-            begin
-            end
-        end
+        assign y = (alu_ctrl == 4'b0001) ? add_y :
+                   (alu_ctrl == 4'b0010) ? mul_y : 8'd0;
 
-        always @ (*)
-        begin
-            case(alu_ctrl)
-            4'b0001:
-            begin
-                next_valid = add_valid;
-                next_out = add_y;
-            end
-            4'b0010:
-            begin
-                next_valid = mul_valid;
-                next_out = mul_y;
-            end
-            default:
-            begin
-                next_valid = 1'b0;
-                next_out = 8'd0;
-            end
-            endcase
-        end
+        assign is_output_valid =
+                   (alu_ctrl == 4'b0001) ? add_valid :
+                   (alu_ctrl == 4'b0010) ? mul_valid : 1'b0;
+
 endmodule
