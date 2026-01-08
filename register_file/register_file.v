262bf271715aadd0c82b1cd2d5e0ef593c2d233c
Szymon <szymon.ozog@gmail.com>
2026-01-08 08:11:00 +0100

    Register file

---
+++ register_file/register_file.v
@@ -0,0 +1,32 @@
+module reg_file(
+        input wire[7:0] r1_addr,
+        input wire[7:0] r2_addr,
+        input wire[7:0] write_addr,
+        input wire[7:0] write_data,
+        input wire write_ctrl,
+        input wire clock,
+        output wire[7:0] r1_out,
+        output wire[7:0] r2_out
+        );
+
+        parameter CAP = 65536;
+
+        reg[7:0] r1_out_next;
+        reg[7:0] r2_out_next;
+
+        reg[CAP][7:0] registers;
+
+        assign r1_out = r1_out_next;
+        assign r2_out = r2_out_next;
+
+        always @ (posedge clock)
+        begin
+            if(write_ctrl)
+            begin
+                registers[write_addr] = write_data;
+            end
+            r1_out_next = registers[r1_addr];
+            r2_out_next = registers[r2_addr];
+        end
+
+endmodule
