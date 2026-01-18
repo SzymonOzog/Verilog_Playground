bac9bd1b2bc0f98f55b9b2055551d32c99608663
Szymon <szymon.ozog@gmail.com>
2026-01-18 08:29:12 +0100

    16 bit register file

--- register_file/register_file.v
+++ register_file/register_file.v
@@ -30,3 +30,36 @@ module reg_file(
         end
 
 endmodule
+
+module reg_file_16b(
+        input wire[15:0] r1_addr,
+        input wire[15:0] r2_addr,
+        input wire[15:0] write_addr,
+        input wire[15:0] write_data,
+        input wire write_ctrl,
+        input wire clock,
+        output wire[15:0] r1_out,
+        output wire[15:0] r2_out
+        );
+
+        parameter CAP = 65536;
+
+        reg[15:0] r1_out_next;
+        reg[15:0] r2_out_next;
+
+        reg[CAP][15:0] registers;
+
+        assign r1_out = r1_out_next;
+        assign r2_out = r2_out_next;
+
+        always @ (posedge clock)
+        begin
+            if(write_ctrl)
+            begin
+                registers[write_addr] = write_data;
+            end
+            r1_out_next = registers[r1_addr];
+            r2_out_next = registers[r2_addr];
+        end
+
+endmodule
--- register_file/tb.v
+++ register_file/tb.v
@@ -1,20 +1,28 @@
 module tb;
+
     reg[7:0] r1_addr;
     reg[7:0] r2_addr;
-
     reg[7:0] write_addr;
     reg[7:0] write_data;
-    
     wire[7:0] r1_out;
     wire[7:0] r2_out;
-
     reg[7:0] expected1;
     reg[7:0] expected2;
+
+    reg[15:0] r1_addr_16;
+    reg[15:0] r2_addr_16;
+    reg[15:0] write_addr_16;
+    reg[15:0] write_data_16;
+    wire[15:0] r1_out_16;
+    wire[15:0] r2_out_16;
+    reg[15:0] expected1_16;
+    reg[15:0] expected2_16;
 
     reg clock;
     reg write;
 
     reg_file r_file(r1_addr, r2_addr, write_addr, write_data, write, clock, r1_out, r2_out);
+    reg_file_16b r_file_16(r1_addr_16, r2_addr_16, write_addr_16, write_data_16, write, clock, r1_out_16, r2_out_16);
 
     always #1 clock = ~clock;
 
@@ -52,6 +60,28 @@ module tb;
         assert(r2_out === expected2) else $fatal(1, "wrong register read data, expected %b, got %b", 
             expected2, r2_out);
 
+        write_addr_16 = 16'hF00D; 
+        write_data_16 = 16'hABCD;
+        r1_addr_16 = write_addr_16;
+
+        #2 assert(r1_out_16 === write_data_16) else 
+            $fatal(1, "16-bit: wrong read data, expected %h, got %h", write_data_16, r1_out_16);
+
+        write_addr_16 = 16'h1111;
+        write_data_16 = 16'h1234;
+        r1_addr_16 = write_addr_16;
+        expected1_16 = write_data_16;
+
+        #2 write_addr_16 = 16'hFFFF;
+        write_data_16 = 16'h5678;
+        r2_addr_16 = write_addr_16;
+        expected2_16 = write_data_16;
+
+        #2 assert(r1_out_16 === expected1_16) else $fatal(1, "wrong register read data, expected %b, got %b", 
+            expected1_16, r1_out_16);
+        assert(r2_out_16 === expected2_16) else $fatal(1, "wrong register read data, expected %b, got %b", 
+            expected2_16, r2_out_16);
+        
         
         $display("All test passed");
         $finish;
