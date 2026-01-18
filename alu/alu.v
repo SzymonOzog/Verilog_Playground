811df400e2bdcba03117bf1e66e8b4da3a300097
Szymon <szymon.ozog@gmail.com>
2026-01-18 08:20:47 +0100

    BF16 alu

--- alu/alu.v
+++ alu/alu.v
@@ -1,4 +1,4 @@
-module alu(
+module alu_e4m3(
         input wire[7:0] a,
         input wire[7:0] b,
         input wire[3:0] alu_ctrl,
@@ -16,6 +16,34 @@ module alu(
 
         float_adder_e4m3 adder(a, b, clock, reset, add_y, add_valid);
         float_multiplier_e4m3 multiplier(a, b, clock, reset, mul_y, mul_valid);
+
+        assign y = (alu_ctrl == 4'b0001) ? add_y :
+                   (alu_ctrl == 4'b0010) ? mul_y : 8'd0;
+
+        assign is_output_valid =
+                   (alu_ctrl == 4'b0001) ? add_valid :
+                   (alu_ctrl == 4'b0010) ? mul_valid : 1'b0;
+
+endmodule
+
+module alu_bf16(
+        input wire[15:0] a,
+        input wire[15:0] b,
+        input wire[3:0] alu_ctrl,
+        input wire clock,
+        input wire reset,
+        output wire[15:0] y,
+        output wire is_output_valid
+        );
+
+        wire[15:0] mul_y;
+        wire mul_valid;
+        
+        wire[15:0] add_y;
+        wire add_valid;
+
+        float_adder_bf16 adder(a, b, clock, reset, add_y, add_valid);
+        float_multiplier_bf16 multiplier(a, b, clock, reset, mul_y, mul_valid);
 
         assign y = (alu_ctrl == 4'b0001) ? add_y :
                    (alu_ctrl == 4'b0010) ? mul_y : 8'd0;
--- alu/tb.v
+++ alu/tb.v
@@ -8,7 +8,15 @@ module tb;
     reg is_output_valid;
     reg[3:0] alu_ctrl;
 
-    alu al(a, b, alu_ctrl, clock, reset, y, is_output_valid);
+    alu_e4m3 al(a, b, alu_ctrl, clock, reset, y, is_output_valid);
+
+    reg[15:0] a_bf16;
+    reg[15:0] b_bf16;
+    wire[15:0] y_bf16;
+    reg[15:0] expected_bf16;
+    reg is_output_valid_bf16;
+
+    alu_bf16 alu_bf16(a_bf16, b_bf16, alu_ctrl, clock, reset, y_bf16, is_output_valid_bf16);
 
     always #1 clock = ~clock;
 
@@ -89,6 +97,54 @@ module tb;
         #10 assert(y === expected & is_output_valid) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, expected);
 
+        a_bf16 = 16'h3f80;
+        b_bf16 = 16'hbf80;
+        expected_bf16 = 16'h0;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'h0;
+        b_bf16 = 16'h0;
+        expected_bf16 = 16'h0;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'hbf80;
+        b_bf16 = 16'hbf80;
+        expected_bf16 = 16'hc000;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'hbf40;
+        b_bf16 = 16'h3fe0;
+        expected_bf16 = 16'h3f80;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'h4348;
+        b_bf16 = 16'h3a83;
+        expected_bf16 = 16'h4348;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'h3fff;
+        b_bf16 = 16'h3fe0;
+        expected_bf16 = 16'h4070;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
         // FP8 MULTIPLY
         #1 alu_ctrl = 4'b0010;
 
@@ -139,6 +195,55 @@ module tb;
         #1 reset = 1'b0;
         #10 assert(y === expected & is_output_valid) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b", 
             a, b, y, expected);
+        
+        a_bf16 = 16'h3f80;
+        b_bf16 = 16'hbf80;
+        expected_bf16 = 16'hbf80;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'h0;
+        b_bf16 = 16'h0;
+        expected_bf16 = 16'h0;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+        
+        a_bf16 = 16'hbf80;
+        b_bf16 = 16'hbf80;
+        expected_bf16 = 16'h3f80;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'hbf40;
+        b_bf16 = 16'h3fe0;
+        expected_bf16 = 16'hbfa8;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'h4348;
+        b_bf16 = 16'h3a83;
+        expected_bf16 = 16'h3e4d;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
+        a_bf16 = 16'h3fff;
+        b_bf16 = 16'h3fe0;
+        expected_bf16 = 16'h405f;
+        #1 reset = 1'b1;
+        #1 reset = 1'b0;
+        #10 assert(y_bf16 === expected_bf16 & is_output_valid_bf16) else $fatal(1, "wrong output for a %b b %b y=%b, expected %b",
+                                                         a_bf16, b_bf16, y_bf16, expected_bf16);
+
 
         
         $display("All test passed");
