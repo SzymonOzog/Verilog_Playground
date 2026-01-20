721a8b620616d58e0141a948bc7bdbeb4a17114f
Szymon <szymon.ozog@gmail.com>
2026-01-20 07:41:27 +0100

    remove the need for states and is_output_valid

--- float_multiplier/float_multiplier.v
+++ float_multiplier/float_multiplier.v
@@ -117,20 +117,12 @@ module float_multiplier_bf16(
         wire[7:0] b_m = {1'b1, b[6:0]};
 
         reg[7:0] y_e;
-        reg[7:0] y_e_next;
 
         reg[6:0] y_m;
-        reg[6:0] y_m_next;
         wire[15:0] y_m_mul;
 
-        reg[1:0] curr_state;
-        reg[1:0] next_state;
-
-        reg next_valid;
-        reg valid;
-        reg round;
-
         reg[7:0] m_discard;
+        reg round;
         wire G;
         wire R;
         wire S;
@@ -138,75 +130,40 @@ module float_multiplier_bf16(
         assign R = m_discard[6];
         assign S = m_discard[5] | m_discard[4] | m_discard[3] | m_discard[2] | m_discard[1] | m_discard[0];
 
-        parameter MUL = 2'd1;
-        parameter ROUND = 2'd2;
         parameter BIAS = 8'd127;
 
         assign y_m_mul = (a_m * b_m);
 
-        always @ (posedge clock or posedge reset)
-        begin
-            if (reset)
-            begin
-                curr_state <= MUL;
-                next_state <= MUL;
-                y_e <= 7'd0;
-                y_m <= 6'd0;
-                y_e_next <= 7'd0;
-                y_m_next <= 6'd0;
-                valid <= 1'b0;
-                next_valid <= 1'b0;
-                m_discard <= 8'b0;
-                round <= 1'b0;
-            end
-            else
-            begin
-                curr_state <= next_state;
-                y_m <= y_m_next;
-                y_e <= y_e_next;
-                valid <= next_valid;
-            end
-        end
-
         always @ (*)
         begin
-            case(curr_state)
-                MUL:
+            if (a == 16'h00 || a == 16'h80 || 
+                b == 16'h00 || b == 16'h80 )
+            begin
+                y_e = 7'd0;
+                y_m = 8'd0;
+            end
+            else 
+            begin
+                y_e = a_e + b_e - BIAS;
+                if(y_m_mul[15])
                 begin
-                    if (a == 16'h00 || a == 16'h80 || 
-                        b == 16'h00 || b == 16'h80 )
-                    begin
-                        y_e_next = 7'd0;
-                        y_m_next = 8'd0;
-                        next_valid = 1'b1;
-                    end
-                    else 
-                    begin
-                        y_e_next = a_e + b_e - BIAS;
-                        if(y_m_mul[15])
-                        begin
-                            y_m_next = y_m_mul[14:8];
-                            m_discard[7:0] = y_m_mul[7:0];
-                            y_e_next = y_e_next + 1'b1;
-                        end
-                        else
-                        begin
-                            y_m_next = y_m_mul[13:7];
-                            m_discard[7:1] = y_m_mul[6:0];
-                            y_e_next = y_e_next;
-                        end
-                        round = G & (R | S | y_m_next[0]);
-                        y_m_next = y_m_next + round;
-                        next_state = ROUND;
-                        next_valid = 1'b1;
-                    end
+                    y_m = y_m_mul[14:8];
+                    m_discard[7:0] = y_m_mul[7:0];
+                    y_e = y_e + 1'b1;
                 end
-
-            endcase
+                else
+                begin
+                    y_m = y_m_mul[13:7];
+                    m_discard[7:1] = y_m_mul[6:0];
+                    y_e = y_e;
+                end
+                round = G & (R | S | y_m[0]);
+                y_m = y_m + round;
+            end
         end
 
         assign y[15] = a_s ^ b_s;
         assign y[14:7] = y_e;
         assign y[6:0] = y_m[6:0];
-        assign is_output_valid = valid;
+        assign is_output_valid = 1'b1;
 endmodule
